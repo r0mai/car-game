c7929c646e7458bd4dd02b04268364b973b0e2dc
Andras Kucsma <andras.janos.kucsma@ericsson.com>
2014-02-05 13:21:57 +0100

    Removed const-s from Car's members, so default assignment operator can be generated

--- src/Car.hpp
+++ src/Car.hpp
@@ -48,34 +48,34 @@ private:
 
 	void updateCorners();
 
-	const float cDrag = 0.5;
-	const float cRollingResistance = 14.2;
-	const float fEngine = 2000.0;
-	const float fBrake = 30000.0;
-	const float gravity = 9.8; //m/s^2
-	const float transMissionEfficiency = 0.7;
-	const float gearRatio = 2.1;
-	const float differentialRatio = 3.42;
-	const float wheelRadius = 0.34; //m
-	const float mass = 1500; //kg
-	const float maxTurnAngle = 0.52; //radians ~= 30 degrees
+	float cDrag = 0.5;
+	float cRollingResistance = 14.2;
+	float fEngine = 2000.0;
+	float fBrake = 30000.0;
+	float gravity = 9.8; //m/s^2
+	float transMissionEfficiency = 0.7;
+	float gearRatio = 2.1;
+	float differentialRatio = 3.42;
+	float wheelRadius = 0.34; //m
+	float mass = 1500; //kg
+	float maxTurnAngle = 0.52; //radians ~= 30 degrees
 
 	//CM == Center of Mass
 	//CG == Center of Gravity
-	const float rearWheelCMDistance = 1.0;
-	const float frontWheelCMDistance = 1.5;
-	const float rearCMDistance = 1.2;
-	const float frontCMDistance = 1.8;
-	const float heightOfCG = 1.5;
-	const float wheelBase = rearCMDistance + frontCMDistance;
-	const float carWidth = 1.4;
+	float rearWheelCMDistance = 1.0;
+	float frontWheelCMDistance = 1.5;
+	float rearCMDistance = 1.2;
+	float frontCMDistance = 1.8;
+	float heightOfCG = 1.5;
+	float wheelBase = rearCMDistance + frontCMDistance;
+	float carWidth = 1.4;
 
-	const float throttleIncreaseSpeed = 1.5;
-	const float throttleDecreaseSpeed = 1.3;
-	const float brakeIncreaseSpeed = 1.9;
-	const float brakeDecreaseSpeed = 1.7;
+	float throttleIncreaseSpeed = 1.5;
+	float throttleDecreaseSpeed = 1.3;
+	float brakeIncreaseSpeed = 1.9;
+	float brakeDecreaseSpeed = 1.7;
 
-	const float turnSpeed = 6.5;
+	float turnSpeed = 6.5;
 
 	float throttleLevel = 0.0; // [0, 1]
 	float brakeLevel = 0.0; // [0, 1]
