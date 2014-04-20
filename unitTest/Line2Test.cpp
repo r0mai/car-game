480a2d9e328820c05d7e876a3df5f66fcbeb1393
Peter Szabados <kangirigungi@gmail.com>
2014-04-20 16:56:05 +0200

    Added unit test framework

---
+++ unitTest/Line2Test.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+#include "Line2.hpp"
+
+using namespace car;
+
+BOOST_AUTO_TEST_SUITE(Line2Test)
+
+BOOST_AUTO_TEST_CASE(first) {
+	Line2f l;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
