a77e902c37def21222f685fc008f7f3a7a6d08f0
Peter Szabados <kangirigungi@gmail.com>
2014-04-20 17:09:58 +0200

    First unit test for Line2

--- unitTest/Line2Test.cpp
+++ unitTest/Line2Test.cpp
@@ -5,8 +5,14 @@ using namespace car;
 
 BOOST_AUTO_TEST_SUITE(Line2Test)
 
-BOOST_AUTO_TEST_CASE(first) {
-	Line2f l;
+BOOST_AUTO_TEST_CASE(intersects_intersecting_lines) {
+	Line2f line1{{0.f, 0.f}, {1.f, 2.f}};
+	Line2f line2{{1.f, 0.f}, {0.f, 2.f}};
+
+	sf::Vector2f intersectionPoint;
+	BOOST_REQUIRE(intersects(line1, line2, &intersectionPoint));
+	BOOST_CHECK_CLOSE(intersectionPoint.x, 0.5f, 0.001);
+	BOOST_CHECK_CLOSE(intersectionPoint.y, 1.0f, 0.001);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
