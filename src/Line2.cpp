d037d06532e408fa791b9a7ad6850a479768dda5
Peter Szabados <kangirigungi@gmail.com>
2014-04-21 10:37:06 +0200

    Factored out intersectio calculation to class

--- src/Line2.cpp
+++ src/Line2.cpp
@@ -1,73 +1,28 @@
 #include "Line2.hpp"
-#include "mathUtil.hpp"
+#include "LineIntersection.hpp"
 
 namespace car {
 
 namespace {
 
 template <typename T>
-struct LineData {
-	T commonDenominator;
-	T numeratorA;
-	T numeratorB;
-};
-
-template <typename T>
-LineData<T> getLineData(const Line2<T>& line1, const Line2<T>& line2) {
-	LineData<T> result;
-	result.commonDenominator = (float)(line1.end.y - line1.start.y)*(line2.end.x - line2.start.x) -
-									(line1.end.x - line1.start.x)*(line2.end.y - line2.start.y);
-
-	result.numeratorA = (float)(line1.end.x - line1.start.x)*(line2.start.y - line1.start.y) -
-									(line1.end.y - line1.start.y)*(line2.start.x -line1.start.x);
-
-	result.numeratorB = (float)(line2.end.x - line2.start.x)*(line2.start.y - line1.start.y) -
-									(line2.end.y - line2.start.y)*(line2.start.x -line1.start.x);
-
-	return result;
-}
-
-template <typename T>
-struct IntersectionData {
-	sf::Vector2<T> intersectionPoint;
-	T uA;
-	T uB;
-};
-
-template <typename T>
-IntersectionData<T> calculateIntersectionPoint(const Line2<T>& /*line1*/, const Line2<T>& line2,
-		const LineData<T>& data) {
-	IntersectionData<T> result;
-
-	result.uA = data.numeratorA / data.commonDenominator;
-	result.uB = data.numeratorB / data.commonDenominator;
-
-	result.intersectionPoint.x = static_cast<T>(line2.start.x + result.uA * (line2.end.x - line2.start.x));
-	result.intersectionPoint.y = static_cast<T>(line2.start.y + result.uA * (line2.end.y - line2.start.y));
-
-	return result;
-}
-
-template <typename T>
 bool intersectsInfinite(const Line2<T>& line1, const Line2<T>& line2, sf::Vector2<T> *outPtr) {
-	const auto data = getLineData(line1, line2);
+	LineIntersection<T> data{line1, line2};
 
-	if(equals(data.commonDenominator, 0.f))
-	{
-		// The lines are either coincident or parallel
-		// if both numerators are 0, the lines are coincident
-		if(equals(data.numeratorA, 0.f) && equals(data.numeratorB, 0.f))
-		{
-			if (outPtr) {
-				*outPtr = line1.start;
-			}
-			return true; // coincident
+	if (data.isCoincident()) {
+		if (outPtr) {
+			*outPtr = line1.start;
 		}
-		return false; // parallel
+		return true;
+	}
+
+	auto intersectionPoint = data.getIntersectionPoint();
+	if (!intersectionPoint) {
+		return false;
 	}
 
 	if (outPtr) {
-		*outPtr = calculateIntersectionPoint(line1, line2, data).intersectionPoint;
+		*outPtr = *intersectionPoint;
 	}
 
 	return true;
@@ -75,92 +30,31 @@ bool intersectsInfinite(const Line2<T>& line1, const Line2<T>& line2, sf::Vector
 
 template <typename T>
 bool intersects(const Line2<T>& line1, const Line2<T>& line2, sf::Vector2<T> *outPtr) {
-	sf::Vector2<T> out;
-	const auto data = getLineData(line1, line2);
+	LineIntersection<T> data{line1, line2};
 
-	if(equals(data.commonDenominator, 0.f))
-	{
-		// The lines are either coincident or parallel
-		// if both numerators are 0, the lines are coincident
-		if(equals(data.numeratorA, 0.f) && equals(data.numeratorB, 0.f))
-		{
-			// Try and find a common line2.endpoint
-			if(line1.start == line2.start || line1.end == line2.start)
-				out = line2.start;
-			else if(line1.end == line2.end || line1.start == line2.end)
-				out = line2.end;
-			// now check if the two segments are disjunct
-			else if (line1.start.x>line2.start.x && line1.end.x>line2.start.x && line1.start.x>line2.end.x && line1.end.x>line2.end.x)
-				return false;
-			else if (line1.start.y>line2.start.y && line1.end.y>line2.start.y && line1.start.y>line2.end.y && line1.end.y>line2.end.y)
-				return false;
-			else if (line1.start.x<line2.start.x && line1.end.x<line2.start.x && line1.start.x<line2.end.x && line1.end.x<line2.end.x)
-				return false;
-			else if (line1.start.y<line2.start.y && line1.end.y<line2.start.y && line1.start.y<line2.end.y && line1.end.y<line2.end.y)
-				return false;
-			// else the lines are overlapping to some extent
-			else
-			{
-				// find the points which are not contributing to the
-				// common part
-				sf::Vector2<T> maxp;
-				sf::Vector2<T> minp;
-				if ((line2.start.x>line1.start.x && line2.start.x>line1.end.x && line2.start.x>line2.end.x) ||
-						(line2.start.y>line1.start.y && line2.start.y>line1.end.y && line2.start.y>line2.end.y))
-					maxp=line2.start;
-				else if ((line2.end.x>line1.start.x && line2.end.x>line1.end.x && line2.end.x>line2.start.x) ||
-						(line2.end.y>line1.start.y && line2.end.y>line1.end.y && line2.end.y>line2.start.y))
-					maxp=line2.end;
-				else if ((line1.start.x>line2.start.x && line1.start.x>line1.end.x && line1.start.x>line2.end.x) ||
-						(line1.start.y>line2.start.y && line1.start.y>line1.end.y && line1.start.y>line2.end.y))
-					maxp=line1.start;
-				else
-					maxp=line1.end;
-				if (maxp != line2.start && ((line2.start.x<line1.start.x && line2.start.x<line1.end.x && line2.start.x<line2.end.x) ||
-						(line2.start.y<line1.start.y && line2.start.y<line1.end.y && line2.start.y<line2.end.y)))
-					minp=line2.start;
-				else if (maxp != line2.end && ((line2.end.x<line1.start.x && line2.end.x<line1.end.x && line2.end.x<line2.start.x) ||
-						(line2.end.y<line1.start.y && line2.end.y<line1.end.y && line2.end.y<line2.start.y)))
-					minp=line2.end;
-				else if (maxp != line1.start && ((line1.start.x<line2.start.x && line1.start.x<line1.end.x && line1.start.x<line2.end.x)
-						|| (line1.start.y<line2.start.y && line1.start.y<line1.end.y && line1.start.y<line2.end.y)))
-					minp=line1.start;
-				else
-					minp=line1.end;
-
-				// one line is contained in the other. Pick the center
-				// of the remaining points, which overlap for sure
-				out = sf::Vector2<T>();
-				if (line2.start != maxp && line2.start != minp)
-					out += line2.start;
-				if (line2.end != maxp && line2.end != minp)
-					out += line2.end;
-				if (line1.start != maxp && line1.start != minp)
-					out += line1.start;
-				if (line1.end != maxp && line1.end != minp)
-					out += line1.end;
-				out.x = static_cast<T>(out.x/2);
-				out.y = static_cast<T>(out.y/2);
-			}
-
-			if ( outPtr ) {
-				*outPtr = out;
-			}
-			return true; // coincident
+	auto overlapLine = data.getOverlapLine();
+	if (overlapLine) {
+		if ( outPtr ) {
+			*outPtr = (overlapLine->start + overlapLine->end) / static_cast<T>(2);
 		}
 
-		return false; // parallel
+		return true;
 	}
 
-	const auto intersectionData = calculateIntersectionPoint(line1, line2, data);
+	auto intersectionPoint = data.getIntersectionPoint();
+	if (!intersectionPoint) {
+		return false;
+	}
 
-	if(intersectionData.uA < 0.f || intersectionData.uA > 1.f ||
-			intersectionData.uB < 0.f || intersectionData.uB > 1.f) {
+	auto uA = data.getIntersectionPointRatioLine1();
+	auto uB = data.getIntersectionPointRatioLine2();
+
+	if(uA < 0.f || uA > 1.f || uB < 0.f || uB > 1.f) {
 		return false; // Outside the line segment
 	}
 
 	if ( outPtr ) {
-		*outPtr = intersectionData.intersectionPoint;
+		*outPtr = *intersectionPoint;
 	}
 
 	return true;
@@ -168,7 +62,7 @@ bool intersects(const Line2<T>& line1, const Line2<T>& line2, sf::Vector2<T> *ou
 
 template <typename T>
 bool isParallel(const Line2<T>& line1, const Line2<T>& line2) {
-	return equals(getLineData(line1, line2).commonDenominator, 0.f);
+	return LineIntersection<T>{line1, line2}.isParallel();
 }
 
 } //  unnamed namespace
