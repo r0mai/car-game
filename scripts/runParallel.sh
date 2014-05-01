9f0c65068b87b0cc74f53c620139002da9d20b03
Peter Szabados <kangirigungi@gmail.com>
2014-05-01 10:04:43 +0200

    Modified fitness calculation and added runner script

---
+++ scripts/runParallel.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+print_usage() {
+    echo "Run multiple instances of car game."
+    echo "Usage: $0 -h" 
+    echo "       $0 [options]"
+    echo "Options:"
+    echo "    -h        This help message."
+    echo "    -c        Add a new config. The number of parallel runs equals to the"
+    echo "              number of times this parameter is given."
+    echo "    -C        Global config file applied to all runs."
+}
+
+interrupt() {
+    kill $(jobs -p)
+}
+
+# -------- Main body --------
+
+runDir="$(dirname $0)"
+
+globalConfigOption=()
+configs=()
+while getopts ":hC:c:" Option; do
+    case $Option in
+    h) 
+	    print_usage
+	    exit
+	    ;;
+	C)
+	    globalConfigOption=("--config=$OPTARG")
+	    ;;
+	c)
+	    configs+=("--config=$OPTARG")
+	    ;;
+    esac
+done
+
+trap interrupt SIGINT
+trap interrupt SIGHUP
+trap interrupt SIGTERM
+trap interrupt SIGQUIT
+
+for config in "${configs[@]}"; do
+    $runDir/car-game --ai "${globalConfigOption[@]}" "$config" >"$config.out" &
+done
+
+
+
+shift $(($OPTIND - 1))
+
