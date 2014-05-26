e564fc3565bceb41d8c59000d52fa64edaa73005
R0mai <r0maikx02b@gmail.com>
2014-05-26 23:22:59 +0200

    runWithGraphs.sh consolidated

--- runWithGraphs.sh
+++ runWithGraphs.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Please export a directory in which feedgnuplot can be found.
-if [[ -z "$FEEDGNUPLOTDIR"  ]] && ! which feedgnuplot 1>/dev/null 2>&1
-then
-	echo "Please set \$FEEDGNUPLOTDIR to the directory of feedgnuplot!."
-	exit
-fi
-
-if which feedgnuplot
-then
+if which feedgnuplot 1>/dev/null; then
 	feedGnuPlotBinary=`which feedgnuplot`
+elif [[ -z "$FEEDGNUPLOTDIR"  ]]; then
+	feedGnuPlotBinary="$FEEDGNUPLOTDIR/bin/feedgnuplot"
 else
-	feedGnuPlotBinary=$FEEDGNUPLOTDIR/bin/feedgnuplot
+	echo 'Please set $FEEDGNUPLOTDIR to the directory of feedgnuplot, or put it in $PATH'
+	exit 1
 fi
 
 lines=$(tput lines)
@@ -20,7 +15,6 @@ columns=$(tput cols)
 # output format is:
 # Generation: 641, Current best fitness: 23749.9, Population averages: 5833.79,
 
-
 cleanup() {
 	tput rmcup
 }
@@ -28,6 +22,9 @@ cleanup() {
 trap cleanup SIGINT
 
 tput smcup
-$@ | sed -u 's/Generation: \(.*\), Current best fitness: \(.*\), Population averages: \(.*\),/\1 \2 \3/g' | $feedGnuPlotBinary --stream --terminal "dumb $columns,$lines" --lines --domain --title 'Car-game' --xlen 100 --legend 0 'Best Fitness' --legend 1 'Population average'
+$@ | sed -u 's/Generation: \(.*\), Current best fitness: \(.*\), Population averages: \(.*\),/\1 \2 \3/g' | \
+	$feedGnuPlotBinary --stream --terminal "dumb $columns,$lines" \
+	--lines --domain --title 'Car-game' --xlen 100 --legend 0 'Best Fitness' --legend 1 'Population average'
 
 cleanup
+
