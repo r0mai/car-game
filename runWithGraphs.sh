ec317743eb530447e843f8599d1feb9f4f284eba
Matthias Vegh <matyas.vegh@gmail.com>
2014-05-26 23:05:12 +0200

    Reworked 1,2 redirection in runWithGraphs.sh.

--- runWithGraphs.sh
+++ runWithGraphs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Please export a directory in which feedgnuplot can be found.
-if [[ -z "$FEEDGNUPLOTDIR"  ]] && ! which feedgnuplot 2>/dev/null
+if [[ -z "$FEEDGNUPLOTDIR"  ]] && ! which feedgnuplot 2>&1 1>/dev/null
 then
 	echo "Please set \$FEEDGNUPLOTDIR to the directory of feedgnuplot!."
 	exit
@@ -9,7 +9,7 @@ fi
 
 if which feedgnuplot
 then
-	feedGnuPlotBinary=`which feedgnuplot 2>/dev/null`
+	feedGnuPlotBinary=`which feedgnuplot`
 else
 	feedGnuPlotBinary=$FEEDGNUPLOTDIR/bin/feedgnuplot
 fi
